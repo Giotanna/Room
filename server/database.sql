9dca6886e6b8d6cbc58f94b39b4bed2d129dd597
Giotanna Vourla <vourlapi@gmail.com>
2023-06-01 18:17:06 +0300

    changed tables

--- server/database.sql
+++ server/database.sql
@@ -2,8 +2,9 @@ CREATE DATABASE roomies;
 
 CREATE TABLE students(
     student_id SERIAL PRIMARY KEY,
-    name VARCHAR(225),
-    email VARCHAR(225),
-    phone_number VARCHAR(225),
+    first_name VARCHAR(225) NOT NULL,
+    last_name VARCHAR(225) NOT NULL,
+    email VARCHAR(225) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     UNIQUE(email)
 );
